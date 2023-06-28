8b8333d87b1230879b2d1ecc3cc89c1e24f5dd74
Vladislav <vladislafff10@gmail.com>
2023-06-28 18:27:59 +0200

    init

---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const start = async () => {
+	try {
+		app.listen(PORT, () => {
+			console.log(`Server listening on port ${PORT}`);
+		});
+	}
+	catch (err) {
+		console.log(err);
+	}
+};
+
+start();
+
+
+
+
+
+
+
+
+
+
