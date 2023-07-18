f78eac730490a3e7431991cc4a9b830cf1908f8d
Vladislav <vladislafff10@gmail.com>
2023-07-18 22:47:30 +0200

    fix user controller

--- server/src/users/user.controller.ts
+++ server/src/users/user.controller.ts
@@ -55,7 +55,7 @@ export class UserController {
 
 	async login(req: Request, res: Response): Promise<Response> {
 		try {
-			const { username, password } = req.body
+			const { username, password } = req.body;
 			const user = await prisma.user.findUnique({
 				where: {
 					username,
@@ -63,21 +63,24 @@ export class UserController {
 			});
 
 			if (!user) {
-				throw new Error("Invalid credentials");
+				return res.status(401).json({ message: 'Invalid username or password' })
 			}
 
 			const isPasswordValid = await compare(password, user.password);
 
 			if (!isPasswordValid) {
-				throw new Error("Invalid credentials");
+				return res.status(401).json({ message: 'Invalid username or password' })
 			}
+			else {
+				const token = generateJwt(user.id, user.username)
 
-			const result = {
-				id: user.id,
-				username: user.username,
-			};
-			const token = generateJwt(user.id, user.username)
-			return res.status(200).json({ message: "User logged in successfully", user: result, token });
+				const result = {
+					id: user.id,
+					username: user.username,
+					token
+				};
+				return res.status(200).json({ message: "User logged in successfully", result });
+			}
 		} catch (error) {
 			res.status(500).json({ message: error.message });
 		}
